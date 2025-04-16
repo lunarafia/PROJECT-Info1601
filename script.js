8ad91f67d2cfd3fa73dc0c03f07701b5bc7a3994
K31SHON <thomaskeishon1@gmail.com>
2025-04-16 17:33:30 -0400

    Added function to search and display movies

--- script.js
+++ script.js
@@ -30,6 +30,38 @@ async function getCurrMovies() { // Fetches the current movies from the API and 
         `;
     });
 }
+
+async function searchMovies(){
+    const apiKey = await fetchApiKey();
+    options.headers.Authorization = `Bearer ${apiKey}`;
+
+    const query = document.getElementById('searchInput').value.trim();
+    if (!query) return;
+
+    try {
+        let movies = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&language=en-US`, options);
+        let moviesData = await movies.json();
+
+        let list = document.getElementById('movie-list');
+        list.innerHTML = '';
+        moviesData.results.forEach(function (movie){
+            list.innerHTML += `
+                <div>
+                    <h2>${movie.title}</h2>
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                </div>
+            `;
+        });
+    } catch (error){
+        console.log('Error fetching movies:', error);
+    }
+}
+document.getElementById('searchInput').addEventListener('keypress', (event) => {
+    if (event.key === 'Enter'){
+        event.preventDefault();
+        searchMovies();
+    }
+})
 getCurrMovies();
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -134,7 +166,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function checkAuth() {
         const token = localStorage.getItem('token');
-        const movieList = document.getElementById('movie-list');
 
         if (token) {
             toggleSidebar.classList.remove("hidden");
@@ -173,5 +204,6 @@ document.addEventListener("DOMContentLoaded", () => {
         checkAuth();
         sidebar.classList.remove("show");
     });
+    checkAuth();
 });
 
